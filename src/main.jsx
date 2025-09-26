b35644cd0e7564668f56dfa542b55ac6e390b120
Yaseen <2300032309@kluniversity.in>
2025-09-27 02:51:03 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { CartProvider } from "./context/CartContext";
-
+import { HashRouter } from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
-  <CartProvider>
-  <App />
-</CartProvider>
+ <StrictMode>
+    <HashRouter>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </HashRouter>
+  </StrictMode>
 )
